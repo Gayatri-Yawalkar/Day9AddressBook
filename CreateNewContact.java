a77f967609fcdee44030ceb34b537c5a1959fae5
unknown <gayoyawalkar@gmail.com>
2021-10-05 20:25:20 +0530

    Added:CreateNewContact Uc1

---
+++ CreateNewContact.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbook;
+//Uc1
+import java.util.ArrayList;
+public class CreateNewContact {
+	ArrayList<Contacts> arrayList;
+	public CreateNewContact() {
+		arrayList=new ArrayList<>();
+	}
+	public static void main(String[] args) {
+		CreateNewContact addressbook=new CreateNewContact();
+	}
+}
